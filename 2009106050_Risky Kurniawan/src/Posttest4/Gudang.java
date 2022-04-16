db1be977cc2fffdea3d858ec549c96b9a91cf9c1
riskykrnawan <riskykrnawan@gmail.com>
2022-04-16 16:31:56 +0800

    2009106050_Risky Kurniawan_Posttest4

---
+++ 2009106050_Risky Kurniawan/src/Posttest4/Gudang.java
@@ -0,0 +1,34 @@
+
+package Posttest3;
+
+import java.util.UUID;
+
+public class Gudang {
+    private String nama, alamat;
+    private final UUID uuid = UUID.randomUUID();
+    private final String id = "Gudang-" + uuid.toString();
+
+    // constructor
+    public Gudang(
+        String nama,
+        String alamat
+    ){
+        this.nama = nama;
+        this.alamat = alamat;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public String getAlamat(){
+        return alamat;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+}
---
+++ 2009106050_Risky Kurniawan/src/Posttest4/Supplier.java
@@ -0,0 +1,49 @@
+
+package Posttest3;
+
+import java.util.UUID;
+
+public class Supplier {
+    private String nama, alamat, noTelepon, email;
+    private final UUID uuid = UUID.randomUUID();
+    private final String id = "Gudang-" + uuid.toString();
+    
+    public Supplier(
+       String nama,
+       String alamat,
+       String noTelepon,
+       String email
+    ) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.noTelepon = noTelepon;
+        this.email = email;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public String getAlamat(){
+        return alamat;
+    }
+    public String getNoTelepon(){
+        return noTelepon;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    public void setNoTelepon(String noTelepon) {
+        this.noTelepon = noTelepon;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
